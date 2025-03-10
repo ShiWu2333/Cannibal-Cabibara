449cbc25da371bb537f496d540fc268b31bff1d6
ShiWu2333 <96139857d@gmail.com>
2025-03-11 07:16:08 +0800

    设置室内进出口

---
+++ Assets/Script/TeleportTrigger.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TeleportTrigger : MonoBehaviour
+{
+    [SerializeField] private Transform teleportDestination; // 传送目标点
+    [SerializeField] private GameObject loadingScreen; // Loading UI
+    [SerializeField] private float loadingTime = 2f; // Loading 屏幕持续时间
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // 仅玩家触发
+        {
+            StartCoroutine(TeleportPlayer(other.gameObject));
+        }
+    }
+
+    private IEnumerator TeleportPlayer(GameObject player)
+    {
+        if (loadingScreen != null)
+        {
+            loadingScreen.SetActive(true); // 显示 Loading 画面
+        }
+
+        yield return new WaitForSeconds(loadingTime); // 等待指定时间
+
+        if (teleportDestination != null)
+        {
+            player.transform.position = teleportDestination.position; // 传送玩家
+        }
+        else
+        {
+            Debug.LogWarning("Teleport destination not set!");
+        }
+
+        if (loadingScreen != null)
+        {
+            loadingScreen.SetActive(false); // 关闭 Loading 画面
+        }
+    }
+}
